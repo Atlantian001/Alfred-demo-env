8822be0faa1e6e336866fad944e083da6e9ffabd
Atlantian001 <107255128+Atlantian001@users.noreply.github.com>
2026-02-09 18:10:58 +0400

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AlfredCommandCenter from './alfred-command-center.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AlfredCommandCenter />
+  </React.StrictMode>,
+)
